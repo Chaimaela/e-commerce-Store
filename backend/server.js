79fd9ad50df92ad734d33bcce4f9461686f51572
chaimaela <laaguidichaimae@gmail.com>
2025-01-19 21:12:33 +0100

    Payment features with Stripe

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import authRoutes from './routes/auth.route.js'
 import productRoutes from './routes/product.route.js'
 import cartRoutes from './routes/cart.route.js'
 import couponRoutes from './routes/coupon.route.js'
+import paymentRoutes from './routes/payment.route.js'
+
 
 
 
@@ -19,6 +21,8 @@ app.use("/api/auth", authRoutes)
 app.use("/api/products", productRoutes)
 app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes)
+app.use("/api/payments", paymentRoutes)
+
 
 
 
