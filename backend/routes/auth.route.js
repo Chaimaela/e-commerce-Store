d8c6dbb8a845fab223451738ed69b13177ef4b68
chaimaela <laaguidichaimae@gmail.com>
2025-02-14 21:49:35 +0100

    admin Dashboard

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { login, logout, signup,refreshToken, getProfile } from "../controllers/auth.controller.js"
+import { protectRoute } from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 
@@ -10,7 +11,7 @@ router.post("/login", login )
 router.post("/logout", logout)
 
 router.post("/refresh-token", refreshToken)
-router.get("/profile", getProfile)
+router.get("/profile",protectRoute, getProfile)
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import productRoutes from './routes/product.route.js'
 import cartRoutes from './routes/cart.route.js'
 import couponRoutes from './routes/coupon.route.js'
 import paymentRoutes from './routes/payment.route.js'
+import analyticsRoutes from "./routes/analytics.route.js";
 
 
 
@@ -22,6 +23,8 @@ app.use("/api/products", productRoutes)
 app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes)
 app.use("/api/payments", paymentRoutes)
+app.use("/api/analytics", analyticsRoutes);
+
 
 
 
